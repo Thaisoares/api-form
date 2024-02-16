2ae19002d30efe89c114d8f36696f420c9a4c0fa
Thaisoares <thais.soares.lamas@gmail.com>
2024-02-16 15:21:53 -0300

    feat: update surveyMongoRepository to have loadAll

--- src/infra/db/mongodb/survey-repository/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-repository/survey-mongo-repository.spec.ts
@@ -2,6 +2,7 @@ import { MongoHelper } from '../helpers/mogo-helper'
 import { type Collection } from 'mongodb'
 import { SurveyMongoRepository } from './survey-mongo-repository'
 import { type AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { ObjectId } from 'mongodb'
 
 let surveysCollection: Collection
 
@@ -11,7 +12,7 @@ const survey: AddSurveyModel = {
     image: 'image',
     answer: 'answer'
   }, {
-    answer: 'answer'
+    answer: 'answer2'
   }],
   date: new Date()
 }
@@ -43,5 +44,22 @@ describe('Survey Mongo Repository', () => {
     await sut.add(survey)
     const currentCount = await surveysCollection.countDocuments()
     expect(currentCount).toBe(beforeCount + 1)
+  })
+
+  test('Should return surveys on success of loadAll', async () => {
+    const sut = makeSut()
+    await surveysCollection.insertOne({ ...survey, _id: new ObjectId('65c3ce0390cbb2ba1aee5db1') })
+    await surveysCollection.insertOne({ ...survey, _id: new ObjectId('65c3ce0390cbb2ba1aee5db2') })
+    const surveys = await sut.loadAll()
+    expect(surveys).toHaveLength(2)
+    expect(surveys[0]).toHaveProperty('question')
+    expect(surveys[0]).toHaveProperty('answers')
+    expect(surveys[0]).toHaveProperty('date')
+  })
+
+  test('Should return empty list when no survey on db', async () => {
+    const sut = makeSut()
+    const surveys = await sut.loadAll()
+    expect(surveys).toHaveLength(0)
   })
 })
--- src/infra/db/mongodb/survey-repository/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey-repository/survey-mongo-repository.ts
@@ -1,8 +1,9 @@
 import { type AddSurveyModel, type AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { type SurveyModel, type LoadSurveysRepository } from '../../../../data/usecases/load-surveys/db-load-survey-protocols'
 import { InsertError } from '../errors'
 import { MongoHelper } from '../helpers/mogo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<null> {
     const surveysCollection = await MongoHelper.getCollection('surveys')
     const result = await surveysCollection.insertOne(surveyData)
@@ -10,5 +11,11 @@ export class SurveyMongoRepository implements AddSurveyRepository {
       return null
     }
     throw new InsertError('surveys')
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveysCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveysCollection.find({}).toArray()
+    return surveys.map(survey => MongoHelper.map(survey))
   }
 }
